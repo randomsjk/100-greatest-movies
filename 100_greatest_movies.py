63b1ee85915d9c180b5228a985520b2c3b9352dc
randomsjk <169779599+randomsjk@users.noreply.github.com>
2024-06-10 11:02:10 +0100

    changed file name so has .py at end

--- 100_greatest_movies.py
+++ 100_greatest_movies.py
@@ -3,17 +3,17 @@
 from bs4 import BeautifulSoup # imports BeautifulSoup library
 import requests # imports requests library
 
-html_text = requests.get("https://www.empireonline.com/movies/features/best-movies-2/").text #.get method grabs text from web page
+html_text = requests.get("https://www.empireonline.com/movies/features/best-movies-2/").text # .get method grabs html text from web page
 souped_html = BeautifulSoup(html_text,'lxml') # creates beautiful soup object from html text using lxml
 
 movies = souped_html.find_all('h3', class_="listicleItem_listicle-item__title__BfenH") # find_all method grabs all elements with defined conditions and puts them in ResultSet object
-movies.reverse() # reverses the order of elements in ResultSet object
-movies_list = [] # initialises list
+movies.reverse() # reverses the order of elements in ResultSet object, which acts a bit like a list
+movies_list = [] # initialises a normal list variable
 
-for movie in movies: # iterates through ResultSet elements
+for movie in movies: # iterates through ResultSet elements in ResultSet object 'movies'
     movies_list.append(movie.text.strip()) # builds list of strings from ResultSet
-for x in range(len(movies_list)): # iterates through list allowing for editing of elements in the loop
-   movies_list[x] = movies_list[x][movies_list[x].find(" ")+1:].strip() # formats strings so it is only the movie names, uses string splicing
+for i in range(len(movies_list)): # iterates through list allowing for editing of list elements in the loop
+   movies_list[i] = movies_list[i][movies_list[i].find(" ")+1:].strip() # formats strings so it is only the movie names, uses string splicing
 
 with open("greatest_movies.txt", "w", newline="", encoding="utf-8") as txtfile: # opens text file in write mode
   for i in range(len(movies_list)): # iterates through list in a way which keeps track of the number for numbering list
